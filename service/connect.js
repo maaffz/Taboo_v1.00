b5b28f8ca91a001b81cfc91378c082e32df56a1e
Marcel Affonso <maaffz@gmail.com>
2022-06-21 19:12:32 +1000

    final versio - mobile ip

--- service/connect.js
+++ service/connect.js
@@ -1,5 +1,6 @@
 import react from "react";
-const API_URL='http://192.168.0.13:8000';
+const API_URL='http://172.20.10.4:8000';
 export const LogIn_URL=`${API_URL}/api/user/login/`;
 export const Register_URL=`${API_URL}/api/user/create/`;
 // http://172.17.120.83:8000/api/user/create/
+//172.20.10.4
